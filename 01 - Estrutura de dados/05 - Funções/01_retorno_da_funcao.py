a71b6d9e24f7538dfa25eb36c61328d2e5059636
isabertazza <116034827+isabertazza@users.noreply.github.com>
2024-09-13 10:58:42 -0300

    Update 01_retorno_da_funcao.py

--- 01 - Estrutura de dados/05 - Funções/01_retorno_da_funcao.py
+++ 01 - Estrutura de dados/05 - Funções/01_retorno_da_funcao.py
@@ -2,12 +2,12 @@ def calcular_total(numeros):
     return sum(numeros)
 
 
-def retorna_antecessor_e_sucessor(numero):
+def retorna_antecessor_e_successor(numero):
     antecessor = numero - 1
     sucessor = numero + 1
 
-    return antecessor, sucessor #obs: esses valores são retornados em uma tupla, que é uma estrutura imutável.
+    return antecessor, successor #obs: esses valores são retornados em uma tupla, que é uma estrutura imutável.
 
 
 print(calcular_total([10, 20, 34]))  # 64
-print(retorna_antecessor_e_sucessor(10))  # (9, 11)
+print(retorna_antecessor_e_successor(10))  # (9, 11)
